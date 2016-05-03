748ace33838482939e4d48d4da71649abf6bb539
Jon Malave <jonmalave@gmail.com>
2016-05-02 20:59:40 -0400

    Create clear.php

---
+++ clear.php
@@ -0,0 +1,13 @@
+<?php
+	// clear data
+	function clearData($filePath){
+		$chatLog = fopen($filePath, "a") or die("Unable to open file!");
+		ftruncate($chatLog, 0);
+		fclose($chatLog);
+		return 'done';
+	}
+
+	// invoke clear data
+	$clear= clearData('chat.log');
+	echo $clear;
+?>
